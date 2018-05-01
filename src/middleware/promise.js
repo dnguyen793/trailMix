05d8745b2b67286654f4579d9e48d5ce549e32e7
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-04-30 23:47:11 -0700

    Added a placeholder middleware folder with promise.js

---
+++ src/middleware/promise.js
@@ -0,0 +1,15 @@
+export default store => next => action =>{
+	if(!action.payload || !action.payload.then){
+		console.log('No promise found');
+		return next(action);
+	}
+
+	console.log('Found a promise');
+	action.payload.then(resp => {
+		const newAction = {...action, payload: resp};
+
+		store.dispatch(newAction);
+	});
+	console.log("The promise is: ",action.payload);
+	return action.payload;  //add_form.js line10 is expecting a promise
+}
