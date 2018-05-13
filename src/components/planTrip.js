628fa25a20f2be38144287ae068d05f276711d84
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-13 02:02:41 -0700

    Re-structured planTrip nav-links; Also added styling

--- src/components/planTrip.js
+++ src/components/planTrip.js
@@ -5,7 +5,8 @@ import {getDirections} from '../actions';
 import keys from '../assets/config/apiKeys';
 import Search from './search';
 import Logo from './logo';
-import TrailLinks from './trail-links';
+import { NavLink } from 'react-router-dom';
+// import TrailLinks from './trail-links';
 import Weather from './weather';
 import Details from './details';
 
@@ -62,17 +63,25 @@ class PlanTrip extends Component {
             <div className="plantrip">
                 <Logo logoClass="wholeLogoContainerLite"/>                                       
                 <Search {...this.props} />  
-                <div className="mainContent">                    
+                <div className="mainContent">
                     <div className="mapContainer"> 
                         <div id='mapDirection' className='googleMap'></div>               
                     </div>
-                    <div id="drivingDirectionContainer">
-                        <div>
-                            <TrailLinks lat={this.props.match.params.lat} long={this.props.match.params.long} />
+                    <div className="planTripOptions">
+                        <div className="planTripTabs">
+                            <NavLink activeClassName='active selected' className="tabLinks" to={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/details`}>Trail Detail</NavLink>
+                            <NavLink activeClassName='active selected' className="tabLinks" to={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/directions`}>Directions</NavLink>
+                            <NavLink activeClassName='active selected' className="tabLinks" to={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/weather`}>Weather</NavLink>
+                        </div>                   
+                        <div className="tabContent">
                             <Route path={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/details`} component={Details} />
+                            <Route path={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/directions`}
+                            render= {props =>{
+                                 
+                                return <div id="drivingDirectionContainer"></div> }} />
                             <Route path={`/planTrip/${this.props.match.params.lat}/lat/${this.props.match.params.long}/long/weather`} component={Weather} />
                         </div>
-                    </div>
+                    </div>    
                 </div>
             </div>
         );
