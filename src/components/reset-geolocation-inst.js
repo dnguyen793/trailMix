236c3ee1ddb87f721ca4deca72215f05d893d63c
Duy Nguyen <duyng93@gmail.com>
2018-05-14 17:17:28 -0700

    Added instruction page to help user reset geolocation

---
+++ src/components/reset-geolocation-inst.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ResetInstruction(){
+    return(
+        <div className="reset-instruction">
+            <p className="label">
+                <a target="_blank" href="https://www.lifewire.com/denying-access-to-your-location-4027789">Inaccutate Location?</a>
+            </p>
+        </div>
+    )
+}
+
+export default ResetInstruction;
