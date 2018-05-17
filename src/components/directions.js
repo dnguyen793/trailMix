fa9b7eb6f8c109533b9bfd5a7b9fa7b9f4212251
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-17 00:13:13 -0700

    Removed constructor and unused function in directions.js

--- src/components/directions.js
+++ src/components/directions.js
@@ -5,15 +5,6 @@ import keys from '../assets/config/apiKeys';
 
 
 class DrivingDirections extends Component {
-	
-	constructor(props){
-		super(props);
-		this.state = {
-			location: [],
-            initLat: 0, //Remove the constructor?
-            initLong: 0
-		};
-	}
 
     componentDidMount(){  
         if(Object.keys(this.props.map).length > 0){ 
@@ -21,14 +12,6 @@ class DrivingDirections extends Component {
             this.props.getDirections(this.props.traillat, 
                 this.props.traillong, this.props.initLat, this.props.initLong, this.props.map);
         }
-    }
-    
-    loadJS(src) {
-        var ref = window.document.getElementsByTagName("script")[0];
-        var script = window.document.createElement("script");
-        script.src = src;
-        script.async = true;
-        ref.parentNode.insertBefore(script, ref);
     }
 
     render(){
