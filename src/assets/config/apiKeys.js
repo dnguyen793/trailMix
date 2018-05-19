32f3e7413147678a9da7a4a796df0d0ba5562229
caxpper <34390189+caxpper@users.noreply.github.com>
2018-05-18 17:05:44 -0700

    Delete apiKeys.js

--- src/assets/config/apiKeys.js
+++
@@ -1,7 +0,0 @@
-
-
-const keys = {
-    rei: '200228018-1624833cbe769c8bfc74c57c6193c8d5',
-    google: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDYScuf8sd1NNdQGFoeKVeXLEYNtYPIroU&libraries=places&callback=initMap'
-};
-export default keys;
