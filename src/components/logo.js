c9f4b0caf19c681eb74a465e72fa906e08c305b9
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-19 20:39:30 -0700

    Added tool-tip on Logo. Cleaned up planTrip

--- src/components/logo.js
+++ src/components/logo.js
@@ -10,7 +10,7 @@ class Logo extends Component{
     
     render(){        
         return (  
-            <div className={this.props.logoClass} onClick={this.backToLanding.bind(this)} >
+            <div className={this.props.logoClass} onClick={this.backToLanding.bind(this)} title={'Back to the homepage'}>
                 <div className="logo">
                     <div className="earthContainer">
                         <div className="hikerContainer">
